3563752e56e6cfdd43ebc1576cd447a1cf765240
jamwalneted <susheel@netedsoftech.com>
2024-03-12 17:46:40 +0530

    live feed updated

--- add_company.php
+++ add_company.php
@@ -181,50 +181,7 @@ if(!($_SESSION)){
                   </form>
                  </div>
               </div>
-              <div class="col-lg-3">
-                <div class="left-content rounded">
-                
-                  <h4 class="text-white">Smart Shipment</h4>
-                  <p class="text-white">Let plan your shipment to save cost</p>
-                  <a href="#" class="btn text-white">Tell me more!</a>
-              </div>
-
-              <div class="scrollbar-main mt-3">
-                <div class="mb-3 scrollerContent">
-                  <p class="mb-0">working on that deal</p>
-                  <div class="d-flex justify-content-between">
-                    <small class="text-black">John son</small>
-                    <small class="text-success " style="font-family: fantasy;font-size: 12px;">27/03/2024</small>
-                  </div>
-                </div>
-                <div class="mb-3 scrollerContent">
-                  <p class="mb-0">working on that deal</p>
-                  <div class="d-flex justify-content-between">
-                    <small class="text-black">John son</small>
-                    <small class="text-success " style="font-family: fantasy;font-size: 12px;">27/03/2024</small>
-                  </div>
-                </div>
-                <div class="mb-3 scrollerContent">
-                  <p class="mb-0">working on that deal</p>
-                  <div class="d-flex justify-content-between">
-                    <small class="text-black">John son</small>
-                    <small class="text-success " style="font-family: fantasy;font-size: 12px;">27/03/2024</small>
-                  </div>
-                </div>
-                <div class="mb-3 scrollerContent">
-                  <p class="mb-0">working on that deal</p>
-                  <div class="d-flex justify-content-between">
-                    <small class="text-black">John son</small>
-                    <small class="text-success " style="font-family: fantasy;font-size: 12px;">27/03/2024</small>
-                  </div>
-                </div>
-              </div>
-
-              <div class="msgBox form-group mt-3">
-                <textarea class="form-control" placeholder="Live Feed" name="" id="" cols="30" rows="5"></textarea>
-                <input type="submit" class="btn form-control mt-2">
-              </div>
-              </div>
+              <?php include("livefeed.php");?>
              </div>
           </div>
         </div>
